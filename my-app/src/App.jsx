59c357fd54f8b73b8febfaf844cd78bfb0fe25f1
Pelwis01 <Pelwis01@gmail.com>
2026-02-16 17:39:31 +0000

    Chnaged API to something else

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,7 +9,7 @@ import IconButton from "@mui/material/IconButton";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 const API_URL =
-  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,rain,precipitation,showers,snowfall,wind_speed_10m,wind_direction_10m,wind_gusts_10m";
+  
 
 function Main_Page() {
   useEffect(() => {
@@ -48,21 +48,5 @@ function MenuBar() {
     </Box>
   );
 }
-
-// function Map() {
-//   return (
-//     //<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-//       {/* <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//       <Marker position={[51.505, -0.09]}>
-//         <Popup>
-//           A pretty CSS3 popup. <br /> Easily customizable.
-//         </Popup>
-//       </Marker> */}
-//     //</MapContainer>
-//   );
-// }
 
 export default Main_Page;
